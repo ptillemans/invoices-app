251a876944359b910d7d9e7e5df3a90cd4c22c08
Peter Tillemans <pti@snamellit.com>
2019-05-22 12:25:19 +0200

    support multiple input files

--- invoices-app/app/jobs/fetch_rsync_job.rb
+++ invoices-app/app/jobs/fetch_rsync_job.rb
@@ -18,7 +18,7 @@ class FetchRSyncJob < ActiveJob::Base
 
   def rsync_scans
     # get locally scanned invoices
-    src_dir = InvoiceConfig.get('SRC_DIR')
+    src_dir = InvoiceConfig.get('SRC_DIR').split(',')
     scan_dir = InvoiceConfig.get('SCAN_DIR')
 
     RsyncInterface.update_database(src_dir, scan_dir)
